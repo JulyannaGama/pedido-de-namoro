2eeefe1c9c3842fe637c2794a68593b15edeb1ce
JulyannaGama <julyannagama6@gmail.com>
2023-12-11 20:54:39 -0300

    OK

--- Pedido de namoro/script.js
+++ Pedido de namoro/script.js
@@ -1,19 +1,16 @@
 function showLove() {
-    document.getElementById('question').innerHTML = 'Eu te amo';
-    document.getElementById('yesButton').disabled = true;
-    document.getElementById('noButton').disabled = true;
-  }
-  
-  function moveNoButton() {
-    const noButton = document.getElementById('noButton');
-  
-    // Gere posições X e Y aleatórias
-    const randomX = Math.random() * (window.innerWidth - 100);
-    const randomY = Math.random() * (window.innerHeight - 50);
-  
-    // Use a propriedade style.left e style.top para mover o botão
-    noButton.style.position = 'absolute';
-    noButton.style.left = `${randomX}px`;
-    noButton.style.top = `${randomY}px`;
-  }
-  
+  document.getElementById('question').innerHTML = 'Eu te amo';
+  document.getElementById('yesButton').disabled = true;
+  document.getElementById('noButton').disabled = true;
+}
+
+function moveNoButton() {
+  const noButton = document.getElementById('noButton');
+
+  const randomX = Math.random() * (window.innerWidth - 100);
+  const randomY = Math.random() * (window.innerHeight - 50);
+
+  noButton.style.position = 'absolute';
+  noButton.style.left = `${randomX}px`;
+  noButton.style.top = `${randomY}px`;
+}
